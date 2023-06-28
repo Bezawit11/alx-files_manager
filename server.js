09fb62bdef2e56b940c802c8d21fc80ec22b327a
Bezawit11 <socbeza13@gmail.com>
2023-06-28 11:06:44 -0700

    express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+  
+const app = express();
+const PORT = 5000;
+  
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running on"+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
