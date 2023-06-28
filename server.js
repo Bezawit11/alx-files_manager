5ecc390d35039a576ecb34051c0a417ba24c7dc9
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-28 19:26:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-  
+const routes = require('./routes/index');
+
 const app = express();
 const PORT = 5000;
-  
+
+app.use(express.json());
+app.use('/', routes); 
+
 app.listen(PORT, (error) =>{
     if(!error)
         console.log("Server is Successfully Running on"+ PORT)
