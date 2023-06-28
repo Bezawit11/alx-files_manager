b703572afca103fe6f51f206ebe0e797042e0464
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-28 20:12:21 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,21 @@
-i
+require('dotenv').config()
+var MongoClient = require('mongodb').MongoClient;
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost'
+    const port = process.env.DB_PORT || 27017
+    const database = process.env.DB_DATABASE || 'files_manager'
+    const url = `mongodb://${host}:${port}/${database}`;
+    this.client = MongoClient.connect(url, function(err, db) {
+      console.log("Database created!");
+      db.close();
+    });
+  }
+
+isAlive() {
+  return this.client.isConnected();
+}
+}
+
+export const dbClient = new DBClient();
