773e3d2bef50ea73658669bd738b262eeb199639
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-28 20:47:14 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -5,17 +5,18 @@ import envLoader from './env_loader';
 class DBClient {
   constructor() {
     envLoader();
-    const host = process.env.DB_HOST || 'localhost'
-    const port = process.env.DB_PORT || 27017
-    const database = process.env.DB_DATABASE || 'files_manager'
-    const url = `mongodb://${host}:${port}/${database}`;
-    this.client = mongodb.MongoClient(url, { useUnifiedTopology: true });
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const our_url = `mongodb://${host}:${port}/${database}`;
+    this.client = mongodb.MongoClient(our_url, { useUnifiedTopology: true });
     this.client.connect();
   }
-  
-  const isAlive = () => {
+
+  isAlive() {
     return this.client.isConnected();
   }
 }
 
 export const dbClient = new DBClient();
+export default dbClient;
