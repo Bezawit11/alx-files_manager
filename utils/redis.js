cc1a875e034938e304490ac27b2c1a298e45900f
Bezawit11 <socbeza13@gmail.com>
2023-06-28 10:08:46 -0700

    task 0

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,7 @@
 const redis = require("redis");
 let client = redis.createClient();
 client.on('error', err => console.log('error', err));
-
+var isAlive = false;
+client.on('ready', function(){
+      isAlive = true;
+});
