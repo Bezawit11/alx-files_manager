e8c38c263cef0475469b3ee9b9f947b88eed33ea
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-28 21:46:17 +0100

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -14,7 +14,7 @@ module.exports = {
       res.status(400);
       res.send('Missing password');
     }
-    const a = dbClient.users.findOne({ email });
+    const a = dbClient.users.findOne({'email': email});
     if (a === 'undefined') {
       res.status(400);
       res.send('Already exist');
