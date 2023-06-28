7e5d9a186eaa3e9bcf88fde2005c935b0fdfe2e8
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-28 19:36:37 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 
-const router  = express.Router(); 
+const router  = Express.Router(); 
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
