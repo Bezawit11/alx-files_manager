5b96395867d6d04c1c97b2078f7526af76b1e1ab
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-28 21:28:20 +0100

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,29 @@
+import sha1 from 'sha1';
+import Queue from 'bull/lib/queue';
+import dbClient from '../utils/db';
+
 module.exports = {
   postNew: function(req, res){
-    res.status(200);
-    res.json({ "redis": true, "db": true });
+    const h = req.headers;
+    const email = h.get('email');
+    const password = h.get('password');
+    if (email === 'undefined') {
+      res.status(400);
+      res.send('Missing email');
+    }
+    if (email === 'undefined') {
+      res.status(400);
+      res.send('Missing password');
+    }
+    const a = db.student.find({'email':h.get('email')});
+    if (a === 'undefined') {
+      res.status(400);
+      res.send('Already exist');
+    }
+     const insertion1 = await (await dbClient.usersCollection())
+      .insertOne({ email, password: sha1(password) });
+    const user_Id = insertion1.insertedId.toString();
+    res.status(201);
+    res.json({email, id:user_id});
   }
 };
