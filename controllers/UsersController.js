ae3bc6cf4be953a32c36a575b2f8b95d97b4ce72
Bezawit11 <socbeza13@gmail.com>
2023-07-11 04:19:59 -0700

    task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,6 @@
 import sha1 from 'sha1';
 import Queue from 'bull/lib/queue';
-import dbClient from '../utils/db';
+import dbClient from '../utils/db'; // eslint-disable-line import/no-named-as-default
 
 module.exports = {
   async postNew(req, res) {
