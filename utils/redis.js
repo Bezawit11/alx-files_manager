58baf29411226681c4ecf22b9b28eb0565d59611
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-07-11 07:28:22 +0100

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -3,15 +3,13 @@ import { createClient } from 'redis';
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.is_alive = false;
     this.client.on('error', err => console.log('error', err));
     this.client.on('connect', () => {
-      this.is_alive = true;
     });
   }
 
 isAlive() {
-  return this.is_alive;
+  return this.client.connected;
 }
 }
 
