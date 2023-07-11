08ebf704603df3fa46db805eddc8cd34979a11c3
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-07-11 07:41:37 +0100

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,18 +1,25 @@
 import redis from 'redis';
 import { promisify } from 'util';
 
+/**
+ * Class for performing operations with Redis service
+ */
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
     this.getAsync = promisify(this.client.get).bind(this.client);
-    this.client.on('error', err => console.log('error', err));
+
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+
     this.client.on('connect', () => {
+      // console.log('Redis client connected to the server');
     });
   }
 
   isAlive() {
     return this.client.connected;
-  }
   }
 
   async get(key) {
@@ -24,7 +31,11 @@ class RedisClient {
     this.client.setex(key, duration, value);
   }
 
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
 const redisClient = new RedisClient();
 
 export default redisClient;
-
