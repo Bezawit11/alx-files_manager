e58be955c0709bed0edfe8299955f4e07245b8c0
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-07-11 10:16:37 +0100

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,10 +3,10 @@ import dbClient from '../utils/db';
 module.exports = {
   getStatus: function(req, res){
     res.status(200);
-    res.json({ "redis": true, "db": true });
+    res.json({ "redis": redisClient.isAlive(), "db": dbClient.isAlive() });
   },
-  getStats: function(req, res){
+  async getStats: function(req, res){
     res.status(200);
-    res.send({ users: dbClient.nbUsers(), files: dbClient.nbFiles() });
+    res.send({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
   }
 };
