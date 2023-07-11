9aab2e5896c49568fc203d6ae85e305e5a07e2b9
Bezawit11 <socbeza13@gmail.com>
2023-07-11 02:48:01 -0700

    tasks

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+
 const express = require('express');
 
-const router  = express.Router(); 
+const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users',  UsersController.postNew);
+router.post('/users', UsersController.postNew);
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -5,12 +5,9 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
-app.use('/', routes); 
+app.use('/', routes);
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running on"+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+app.listen(PORT, (error) => {
+  if (!error) console.log(`Server is Successfully Running on${PORT}`);
+  else console.log("Error occurred, server can't start", error);
+});
