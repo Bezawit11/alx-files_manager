04a2b058b4388c4eb629dca00962bc497476a83d
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-07-16 11:49:53 +0100

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,8 @@
 import dbClient from '../utils/db'; // eslint-disable-line import/no-named-as-default
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
+import redisClient from '../utils/redis';
+import userUtils from '../utils/user';
 
 module.exports = {
     getConnect(req, res) {
